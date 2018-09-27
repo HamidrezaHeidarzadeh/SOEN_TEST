7d2b768c79f71e023b7ccd87ab0612ffa1ac3cd2
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-27 12:35:25 -0400

    deleting user2 function

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ console.log("Line One Added");
 console.log("user 2");
 console.log("Line TWO Added");
 console.log("Adding a new line on line 4");
-var user2 = function (test){
-    console.log("user2");
-};
